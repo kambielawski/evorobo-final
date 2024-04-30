6dd52434709a7a261d8cc5420e0b4b378950978f
Kameron Bielawski <kam@ip0af5d1e7.int.uvm.edu>
2024-04-30 13:40:53 -0400

    run_exp script

---
+++ run_exp.py
@@ -0,0 +1,36 @@
+import argparse
+
+from evolution import Evolution
+from robot import SpringRobot
+from simulation import Simulation
+
+'''
+parser = argparse.ArgumentParser()
+parser.add_argument('--exp_file', type=str, required=True)
+
+args = parser.parse_args()
+
+exp_file = args.exp_file
+exp_params = eval(open(exp_file).read())
+
+exp = Evolution(exp_params)
+exp.evolve()
+exp.pickle_evo()
+
+'''
+
+constraints = {
+    'min_spring_length': 1,
+    'max_spring_length': 2,
+    'n_points': 5
+}
+exp_params = {
+    'population_size': 1,
+    'max_generations': 30,
+    'robot_constraints': constraints
+}
+
+########
+
+evo_run = Evolution(exp_params)
+evo_run.evolve()
